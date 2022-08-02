b9a353709df05ddd3dd33c4e7727123ce10f1c4c
Billy.Zheng <vil963@gmail.com>
2022-08-02 18:25:54 +0800

    Refactor

--- src/config/path.cr
+++ src/config/path.cr
@@ -6,4 +6,10 @@ struct Path
   getter admin_company_new = "/admin/companies/new"
   getter admin_company_edit = "/admin/companies/:id/edit"
   getter admin_company_delete = "/admin/companies/:id/delete"
+
+  getter admin_manufactory_index = "admin/manufactories"
+  getter admin_manufactory = "admin/manufactories/:id"
+  getter admin_manufactory_new = "admin/manufactories/new"
+  getter admin_manufactory_edit = "admin/manufactories/:id/edit"
+  getter admin_manufactory_delete = "admin/manufactories/:id/delete"
 end
--- src/operations/save_company.cr
+++ src/operations/save_company.cr
@@ -1,7 +1,5 @@
 class SaveCompany < Company::SaveOperation
-
-  # To save user provided params to the database, you must permit them
-  # https://luckyframework.org/guides/database/saving-records#perma-permitting-columns
-  #
-  # permit_columns column_1, column_2
+  before_save do
+    validate_uniqueness_of name, query: CompanyQuery.new, message: "已经存在"
+  end
 end
