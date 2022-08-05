328e1f14f04e706e4beca140f82cca6799489ef3
Billy.Zheng <vil963@gmail.com>
2022-08-05 15:08:07 +0800

    Update backed_file_system_mounter version.

--- src/config/baked_file_system.cr
+++ src/config/baked_file_system.cr
@@ -1,11 +1,6 @@
 require "baked_file_system_mounter"
 
-BakedFileSystemMounter.assemble(
-  {
-    "public" => "public",
-    "db" => "db"
-  }
-)
+BakedFileSystemMounter.assemble(["public", "db"])
 
 if APP_ENV == "production"
   STDERR.puts "Mounting from baked file system ..."
