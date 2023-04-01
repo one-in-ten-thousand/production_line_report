646ae1ee97f5695276670dd1e7972c721b5ebc8d
Billy.Zheng <vil963@gmail.com>
2023-04-01 14:52:41 +0800

    Add materialize.css materialize.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,4 @@
 // Rails Unobtrusive JavaScript (UJS) is *required* for links in Lucky that use DELETE, POST and PUT.
 // Though it says "Rails" it actually works with any framework.
 require("@rails/ujs").start();
-
+require("./materialize.min")
